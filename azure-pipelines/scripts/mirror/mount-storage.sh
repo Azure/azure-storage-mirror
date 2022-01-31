2e779317ea8899188679374d672e9993716919f4
xumia <xumia@microsoft.com>
2022-01-31 00:06:12 +0000

    Add waiting time after mount

--- azure-pipelines/scripts/mirror/mount-storage.sh
+++ azure-pipelines/scripts/mirror/mount-storage.sh
@@ -11,7 +11,7 @@ if [ -z "$STORAGE_ACCOUNT" ]; then
     exit 1
 fi
 
-if [ "$REMOUNT" == "y" ] && mountpoint $MOUNTPOINT; then
+if [ "$REMOUNT" == "y" ] && mountpoint -q $MOUNTPOINT; then
    echo "umount $MOUNTPOINT"
    sudo umount $MOUNTPOINT
 fi
@@ -29,10 +29,11 @@ if ! mountpoint $MOUNTPOINT; then
     export AZURE_STORAGE_ACCOUNT="$STORAGE_ACCOUNT"
     export AZURE_STORAGE_AUTH_TYPE=MSI
     sudo -E blobfuse "$MOUNTPOINT" --container-name="$CONTAINER_NAME" --tmp-path="$TEMPPATH" -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 -o allow_other
+    sleep 5
 fi
 
 # Validate the mount results
-if ! mount | grep -q "$MOUNTPOINT "; then
+if ! mountpoint -q $MOUNTPOINT; then
     echo "Failed to mount $MOUNTPOINT" 1>&2
     exit 1
 fi
